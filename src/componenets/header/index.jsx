77d80b08ce155831f746bb15ca33f0cfbfdf4754
sumit jaiswal <sumitjaiswal263@gmail.com>
2024-12-01 20:40:50 +0530

    v1.2.2

--- src/componenets/header/index.jsx
+++ src/componenets/header/index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useState } from 'react';
+
 import { Link } from "react-router-dom"; // If you use routing for pages
 import logo from "../../assets/logo-header.png";
 import "./style.scss";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="header">
       <div className="logo">
@@ -11,7 +19,14 @@ const Header = () => {
           <img src={logo} alt="CODADDY" />
         </Link>
       </div>
-      <div className="nav-links">
+
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
         <Link
           to="#"
           className="nav-link"
@@ -19,10 +34,10 @@ const Header = () => {
         >
           Code Editor
         </Link>
-        <Link to="/contests" className="nav-link">
+        <Link to="/contests" className="nav-link" onClick={() => setIsMenuOpen(false)}>
           Codeforces Contests
         </Link>
-        <Link to="/problems" className="nav-link">
+        <Link to="/problems" className="nav-link" onClick={() => setIsMenuOpen(false)}>
           Codeforces Problems
         </Link>
       </div>
